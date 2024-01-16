d614359b401bac4d4e57195773c59007b84afcc5
Poonam Pande <p.pande@earthlink.net>
2024-01-16 15:51:22 -0500

    using class from MDN docs worked but using CLI or function did not work liely because the app vue file has LoremIpsum vs loremIpsum Also still need to figure out how to get lorem: to print before the sentences

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,36 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const config = { port: process.env.PORT || 3000 }
+const morgan = require('morgan');
+
+const LoremIpsum = require("lorem-ipsum").LoremIpsum;
+
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 8,
+    min: 4
+  },
+  wordsPerSentence: {
+    max: 16,
+    min: 4
+  }
+});
+
 
 // middleware
 app.use(cors())
 
-// your API route(s) here
+app.use(morgan("dev"))
 
+// your API route(s) here
+// app.get('/', (req, res) => {
+//   res.send('Lorem Ipsum')
+// })
+
+
+app.get('/lorem/', (req, res) => {
+    res.json({lorem: lorem.generateSentences(2)});
+})
 
 app.get('*', function (req, res) {
   res.status(404).json({ error: 'route not found' })
